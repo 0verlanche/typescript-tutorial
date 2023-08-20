7175fcd945baa1fa5f5718682fce549fc52945b9
supersudouser <supersudousr@gmail.com>
2023-08-20 17:16:06 +0100

    Update .ts file

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -28,3 +28,56 @@ array = ["hello", "bye", "hi"];
 let greet = () => {
   console.log("Hello, World");
 }
+
+let actor: Function;
+
+actor = () =>{
+  console.log('My name is John Staurt');
+}
+
+const addition = (a: number, b: number, c?: number | string): void => {
+  console.log(a+b);
+}
+
+addition(8,0,"o");
+
+type StringOrNum = string | number;
+type ObjWithTypes = {name: string, uid: StringOrNum};
+
+const logDetails = (uid: StringOrNum, item: string) => {
+  console.log("${item} has a uid of ${uid}");
+}
+
+const greeting = (user: ObjWithTypes) => {
+ console.log("${user.name} says hello");
+}
+
+/*Function Sgnatures*/
+
+// example 1
+let sayHello: (a: string, b: string) => void;
+sayHello = (name: string, greeting: string) => {
+  console.log("${name} says ${greeting}");
+}
+
+// example 2
+let calc: (a: number, b:number, c: string) => number;
+calc = (numOne: number, numTwo: number, action: string){
+  if(action === "add"){
+    return numOne + numTwo;
+  }else{
+    return numOne - numTwo
+  }
+}
+
+// example 3
+let log: (obj: {name: string, age: number}) => void;
+
+type ObjWithType = {name: string, age: number};
+log = (ninja: ObjWithType) => {
+  console.log("${ninja.name} is ${ninja.age} years old");
+}
+
+
+
+
